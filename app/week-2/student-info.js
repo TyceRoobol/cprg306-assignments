fe92b37ab5af578a2cdd902b5ad80c8c4871deb0
TyceRoobol <tyce.roobol@edu.sait.ca>
2024-09-23 13:27:20 -0600

    2nd commit
    finished week 2 assignment content

---
+++ app/week-2/student-info.js
@@ -0,0 +1,8 @@
+export default function StudentInfo() {
+    return(
+        <main>
+            <h1>Tyce Roobol</h1>
+            <p><a href="https://github.com/TyceRoobol/cprg306-assignments" alt="repository link">Repository Link</a></p>
+        </main>
+    )
+}
