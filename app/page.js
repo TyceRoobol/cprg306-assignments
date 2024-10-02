54e1fbb3d87af3bcb6b318451702a4cfcdfcd469
TyceRoobol <tyce.roobol@edu.sait.ca>
2024-10-01 22:15:09 -0600

    week 3
    finished week 3 assignment

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ export default function Main() {
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <p><a href="./week-2">week 2</a></p>
+      <p><a href="./week-3">week 3</a></p>
     </main>
   )
 }
---
+++ app/week-3/page.js
@@ -0,0 +1,14 @@
+// page.js
+
+import ItemList from "./item-list";
+
+const Page = () => {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold text-center">Shopping List</h1>
+      <ItemList />
+    </main>
+  );
+};
+
+export default Page;
